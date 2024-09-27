10f2f5910ec9b7ebf8ba2d82ac01e0144090ccf9
Aditya-Srivastava <pks85715@gmail.com>
2024-09-28 00:42:37 +0530

    Landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 export default async function Home() {
- 
   return (
-   <div>
-    Hello World
-   </div>
-   
+    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality
+          <span className="text-blue-600"> digital assets</span>
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-zinc-500">Welcome to DigitalHippo.Every asset on our platform is verified by out team to ensure high quality standards</p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href='/prodcuts' className={buttonVariants({variant:"ocean"})}>Browse Trending</Link>
+          <Button variant="ghost">Our quality promise &rarr;</Button>
+        </div>
+      </div>
+    </div>
   );
 }
