d3c4f9477e36746c0e54d67169b3e23b22e852eb
Aditya-Srivastava <pks85715@gmail.com>
2024-09-28 22:40:55 +0530

    cart items added

---
+++ src/components/Cart.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { ShoppingCart } from "lucide-react"
+import { Sheet, SheetTrigger } from "./ui/sheet"
+
+const Cart = () => {
+    return (
+        <Sheet>
+            <SheetTrigger className="group -m-2 flex items-center p-2">
+                <ShoppingCart aria-hidden='true' className="h-6 w-6 flex-shrink-0 text-gray-500 group-hover:text-gray-600"></ShoppingCart>
+            </SheetTrigger>
+        </Sheet>
+    )
+}
+
+export default Cart
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,78 @@
-import Link from "next/link"
-import { Icons } from "./Icons"
-import NavItems from "./NavItems"
+import Link from "next/link";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
-    return(
-        <div className="bg-white sticky x-50 top-0 inset-x-0 h-16">
-            <header className="realtive bg-white">
-            <div className="maxWidthWrapper mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
-                <div className="border-b border-gray-200 ">
-                    <div className="flex h-16 items-center">
-                        {/* Mobile Navbar */}
-                        <div className="ml-4 flex lg:ml-0">
-                            <Link href="/">
-                            <Icons.logo className="h-10 w-10"/>
-                            </Link>
-                        </div>
-                        <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
-                            <NavItems/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </header>
-        </div>
-    )
-}
+  const user = null;
 
-export default Navbar
+  return (
+    <div className="bg-white sticky x-50 top-0 inset-x-0 h-16">
+      <header className="realtive bg-white">
+        <div className="maxWidthWrapper mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+          <div className="border-b border-gray-200 ">
+            <div className="flex h-16 items-center">
+              {/* Mobile Navbar */}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({ variant: "ocean" })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+                  {user ? null : (
+                    <span
+                      className="h-6 w-px bg-gray-300"
+                      aria-hidden="true"
+                    ></span>
+                  )}
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+                  {user ? (
+                    <span
+                      className="h-6 w-px bg-gray-300"
+                      aria-hidden="true"
+                    ></span>
+                  ) : null}
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-300"
+                        aria-hidden="true"
+                      ></span>
+                    </div>
+                  )}
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
